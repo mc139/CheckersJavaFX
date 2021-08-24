c5d606f5d5b940de7dd214d21765be28836fb076
mc139 <66218036+mc139@users.noreply.github.com>
2021-08-24 18:01:56 +0100

    Update Main.java

--- src/main/java/pl/maciejcieslik/checkers/logic/Main.java
+++ src/main/java/pl/maciejcieslik/checkers/logic/Main.java
@@ -2,9 +2,4 @@ package pl.maciejcieslik.checkers.logic;
 
 public class Main {
 
-//    public static void main(String[] args) {
-//        Board board = new Board();
-//        board.boardRows.
-//
-//    }
 }
